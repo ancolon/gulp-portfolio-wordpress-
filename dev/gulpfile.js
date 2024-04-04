c8b4e40f808560a1945cb6abfd0e6fe15ddb37b0
ankoCode <azkuikoxyzv44@gmail.com>
2024-04-04 22:31:14 +0900

    gulpfileにcompilesassを追加しました。

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -6,3 +6,15 @@ const cssSorter = require("css-declaration-sorter");
 const browser = require("browser-sync");
 
 
+function compileSass(){
+    return gulp.src("../src/sass/**/*.sass")
+    pipe(sass())
+    pipe(postcss([autoprefixer(), cssSorter()]))
+    pipe(gulp.dest("../public/css/"))
+}
+
+function watch(){
+    gulp.watch("../src/sass/**/**.sass", compileSass)
+}
+
+exports.default = watch;
