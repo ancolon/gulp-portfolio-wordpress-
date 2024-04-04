03517a5481afd219607fcb0fc795ff444ce6a1ff
ankoCode <azkuikoxyzv44@gmail.com>
2024-04-04 21:57:30 +0900

    gulpfile.jsにconstを追加しました

---
+++ dev/gulpfile.js
@@ -0,0 +1,8 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssSorter = require("css-declaration-sorter");
+const browser = require("browser-sync");
+
+
