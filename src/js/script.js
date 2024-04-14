aa3d6cd397e6a9e959adfcfbd48b14aed6525a63
ankoCode <azkuikoxyzv44@gmail.com>
2024-04-14 22:32:47 +0900

    twitter(x)ボタンをcontentにて配置しました。そしてparts.htmlにて確認用のダークモード、ライトモードになるボタンを設置しました！

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+const color_btn = document.querySelector('#color-btn');
+
+color_btn.addEventListener('click',()=>{
+    document.body.classList.toggle('dark-theme');
+
+    if(color_btn.textContent === 'ダークモード'){
+        color_btn.textContent = "ライトモード";
+    } else {
+        color_btn.textContent = "ダークモード";
+    }
+})
