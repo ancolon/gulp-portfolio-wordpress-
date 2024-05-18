db1030b8b25fc4383dd3c483d1e5e9750e74c3f8
ankoCode <azkuikoxyzv44@gmail.com>
2024-05-18 11:34:50 +0900

    デザインの調整と一部clamp関数を用いたコーディングを行いました！

--- src/js/script.js
+++ src/js/script.js
@@ -54,18 +54,18 @@ jQuery(window).on('scroll' , function(){
     });
 }
 
-{
+// {
 
-const color_btn = document.querySelector('#color-btn');
+// const color_btn = document.querySelector('#color-btn');
 
-color_btn.addEventListener('click',()=>{
-    document.body.classList.toggle('dark-theme');
+// color_btn.addEventListener('click',()=>{
+//     document.body.classList.toggle('dark-theme');
 
-    if(color_btn.textContent === 'ダークモード'){
-        color_btn.textContent = "ライトモード";
-    } else {
-        color_btn.textContent = "ダークモード";
-    }
-});
+//     if(color_btn.textContent === 'ダークモード'){
+//         color_btn.textContent = "ライトモード";
+//     } else {
+//         color_btn.textContent = "ダークモード";
+//     }
+// });
 
-}
+// }
